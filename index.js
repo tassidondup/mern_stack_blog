7c067ed8a7dbdd20f7beb5bbd948190b95e78f34
Tashi Dhondup <tassidondup@gmail.com>
2020-07-06 08:38:44 +1000

    Added user login middleware

--- index.js
+++ index.js
@@ -33,4 +33,32 @@ app.post("/api/users/register", (req, res) => {
   });
 });
 
+app.post("/api/user/login", (req, res) => {
+  // find email in database
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    // compare the hased password with database hashed password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "Wrong password",
+        });
+    });
+
+    //generate Token
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
+  });
+});
+
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +32,41 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hashedPassword) {
+        if (err) return next(err);
+        user.password = hashedPassword;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = (plainPassword, callback) => {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = (callback) => {
+  let user = this;
+  let token = jwt.sign(user._id.toHexString(), "secret");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
